8221e260b8b1ffc16df78b72c466af8cba660ac6
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2021-08-05 23:44:20 -0700

    add move command
    places built dll into game directory after building

---
+++ move.py
@@ -0,0 +1,8 @@
+#import shutil
+#
+#src = 'DiscoExplorer (Remastered)\\bin\\Debug\\DiscoExplorer (Remastered).dll'
+#dest = 'E:\\SteamLibrary\\steamapps\\common\\Disco Elysium\\BepInEx\\plugins\\DiscoExplorer (Remastered).dll'
+#
+#shutil.copyfile(src, dest)
+
+exit(0)
