d042bb60375952adf80e6847670764c4e2a6c836
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2021-08-30 18:52:09 -0700

    add ESC key support

--- DiscoExplorer (Remastered)/DiscoExplorerComponent.cs
+++ DiscoExplorer (Remastered)/DiscoExplorerComponent.cs
@@ -48,6 +48,19 @@ namespace DiscoExplorer
 
                 Event.current.Use();
             }
+
+            // Esc
+            if (toggle && (Input.GetKeyInt(BepInEx.IL2CPP.UnityEngine.KeyCode.Escape) && Event.current.type == EventType.KeyDown))
+            {
+                toggle = false;
+
+                if (Sunshine.Views.ViewController.Current == Sunshine.Views.ViewType.INVENTORY)
+                {
+                    Sunshine.Views.ViewController.ToggleView(Sunshine.Views.ViewType.INVENTORY, false);
+                }
+
+                Event.current.Use();
+            }
         }
 
         [HarmonyPostfix]
